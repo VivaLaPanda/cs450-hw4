0fd7978baf4ff7184c01fbdcf88cc63a00547b41
Jeffrey Booher-Kaeding <jeffbk95@gmail.com>
2017-12-02 21:14:54 -0800

    here is the lookup function for calculating the memory address. if it returns a neg number, that means it failed.

---
+++ tablelookup.c
@@ -0,0 +1,45 @@
+//
+// Created by Jeffrey  Booher-Kaeding on 12/2/17.
+//
+
+#include "tablelookup.h"
+
+int lookup(int** table, char* offset, int vpn){
+    //table:
+    //[valid][permission][ppn][used]
+
+    //check to make sure its valid
+    if(table[vpn][0] != 0) {
+        //check to see if we have permission
+        if (table[vpn][1] != 0) {
+            //check to see if its used
+            if(table[vpn][3] != 0){
+                //grab the ppn
+                int ppn = table[vpn][3];
+
+                //create a string buffer for our mem maths.
+                char * buffer;
+                buffer = (char*) malloc (64);
+
+                //add out ppn
+                itoa(ppn,buffer,2);
+                //add our offset
+                strcat(buffer,offset);
+
+                //our final int to return
+                long address;
+                address = strtol ( buffer,NULL,2);
+                //return [ppn][offset]
+                return address;
+            } else
+                //not used
+                return -1;
+        } else
+            //no permission
+            return -2;
+    }
+    //not valid
+    return -3;
+
+
+}
---
+++ tablelookup.h
@@ -0,0 +1,23 @@
+//
+// Created by Jeffrey  Booher-Kaeding on 12/2/17.
+//
+
+#ifndef CS450_HW4_TABLELOOKUP_H
+#define CS450_HW4_TABLELOOKUP_H
+
+#include <stdlib.h>
+#include <string.h>
+
+//adress translation
+//given 3 things
+//table
+//offset *array
+//vpn int
+
+//lookup, ppn -> array of bits
+//
+
+int lookup(int** table, char* offset, int vpn);
+
+
+#endif //CS450_HW4_TABLELOOKUP_H
