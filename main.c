f97d3e0c2ee763ac9d4b411af2ff8fb1a6873fd2
Adrian Smith <adrian@smithdev.io>
2017-12-05 18:31:13 -0800

    Adding test case

--- main.c
+++ main.c
@@ -9,11 +9,6 @@ int main(int argc, char *argv[]) {
 	char* offsetStr;
 	offsetStr = itoa(offset, 2);
 	int result = lookup(pageTable, offsetStr, 1);
-	if (!result){
-		printf( "The input is not a valid Sudoku\n");
-	}
-	else{
-		printf("You did it kiddo, you valid Sudoku\n");
-	}
+	
 	return 0;
 }
