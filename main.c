8ff6b51e955e935851966ee9936fb01f3c9ebe00
Jeffrey Booher-Kaeding <jeffbk95@gmail.com>
2017-12-06 12:03:39 -0800

    user should be able to provide his requests, need to re-write parser source

--- main.c
+++ main.c
@@ -4,13 +4,38 @@
 #include "itoa.h"
 
 int main(int argc, char *argv[]) {
+	if (argc < 2)
+	{
+		printf("you need to provide all 2 files \n");
+		return 1;
+	}
+
 	int** pageTable;
 	int* offset =  malloc(sizeof(int));
 	pageTable = ParseFile(pageTable, offset); // Parsefile stores result in pageTable
-	char* offsetStr;
-	offsetStr = itoa(*offset, 2);
-	int result = lookup(pageTable, "00000", 1);
-	printf("%x\n", result);
-	
+	char offsetStr[*offset];
+	char input[256];
+	int num_bits;
+	while (1)
+	{
+
+		printf("please enter your input:");
+		num_bits = scanf("%s",input);
+		printf("\n");
+		char offsetStr[*offset];
+		if ((num_bits - *offset) > 0) {
+			int j = 0;
+			for (int i = num_bits - *offset; i < num_bits; i++)
+			{
+				offsetStr[j] += input[i];
+				j++;
+			}
+
+			int result = lookup(pageTable, offsetStr, 1);
+			printf("%x\n", result);
+		} else
+			break;
+	}
+
 	return 0;
 }
